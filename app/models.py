e273c1bbd4e8fd1da68d9d3c4e48192cab8f3463
Antti Ruonakoski <aruonakoski@gmail.com>
2023-02-18 23:02:22 +0200

    remove unused pandas dtype to SQlAlchemy CamelCase type mapping. Nice try

--- app/models.py
+++ app/models.py
@@ -13,26 +13,6 @@ from sqlalchemy.orm import relationship
 from .database import table_name, metadata, engine
 from sqlalchemy.ext.automap import automap_base
 
-def get_column(dtype: str) -> Column:
-    """Very crude Pandas dtype string alias to SqlAlchemy CamelCase datatype mapping
-    Args:
-        dtype (str): Pandas dtype.astype(str)
-    Returns:
-        Column: SqlAlchemy ORM class
-    """
-
-    if "bool" in dtype:
-        cc_type = Boolean
-    elif "int" in dtype:
-        cc_type = Integer
-    elif "float" in dtype:
-        cc_type = Numeric
-    elif "datetime" in dtype:
-        cc_type = DateTime
-    else:
-        cc_type = String
-    return Column(cc_type)
-
 
 def ReflectedBird():
     metadata.reflect(engine)
